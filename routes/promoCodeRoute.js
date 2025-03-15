3b166d24c99e9328a45355db0c8496813430cd09
0Armaan025 <armaan33000@gmail.com>
2025-03-15 16:20:11 +0530

    add promo code

--- routes/promoCodeRoute.js
+++ routes/promoCodeRoute.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const PromoCode = require("../models/PromoCode");
 const User = require("../models/User");
+const authMiddleWare = require("../middlewares/authMiddleware");
 
 
 router.get("/", async (req, res) => {
@@ -12,10 +13,13 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/create", async (req, res) => {
-    // this will create a promo code of 16 digits
+
     try {
 
-        const code = Math.random().toString(36).substring(2, 18).toUpperCase();
+        const code = [...Array(16)]
+            .map(() => Math.floor(Math.random() * 36).toString(36))
+            .join('')
+            .toUpperCase();
         const expiresAt = new Date();
         expiresAt.setDate(expiresAt.getDate() + 30);
         const promoCode = new PromoCode({
@@ -33,6 +37,51 @@ router.post("/create", async (req, res) => {
             message: "some problem is there, sorry!",
             error: e.message,
         })
+    }
+});
+
+router.post('/apply/:code', authMiddleWare, async (req, res) => {
+    try {
+        const { code } = req.params;
+        const userId = new mongoose.Types.ObjectId(req.user.id);
+        const promoCode = await PromoCode.findOne({ code });
+
+        if (!promoCode) {
+            return res.status(404).json({
+                message: "Invalid promo code",
+            });
+        }
+        else if (promoCode.expiresAt < new Date()) {
+            return res.status(400).json({
+                message: "Promo code expired",
+            });
+        }
+        else {
+
+            const user = await User.findOne({ _id: userId });
+
+            if (!user) {
+                return res.status(404).json({
+                    message: "User not found",
+                });
+            }
+            else {
+                user.promoCodeApplied = code;
+                user.isPremium = true;
+                await user.save();
+                return res.status(200).json({
+                    message: "Promo code applied successfully",
+                });
+            }
+
+
+        }
+    }
+    catch (e) {
+        return res.status(500).json({
+            message: "Some error occurred",
+            error: e.message,
+        });
     }
 });
 
